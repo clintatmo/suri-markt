ee0ba874ec76fc343545a9349ef0f9663c7928bc
clintatmo <clintatmo@gmail.com>
2017-10-25 14:12:17 -0300

    Added zizaco entrust

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Role;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,17 @@ class HomeController extends Controller
      */
     public function index()
     {
+//        $owner = new Role();
+//        $owner->name         = 'owner';
+//        $owner->display_name = 'Project Owner'; // optional
+//        $owner->description  = 'User is the owner of a given project'; // optional
+//        $owner->save();
+
+//        $admin = new Role();
+//        $admin->name         = 'admin';
+//        $admin->display_name = 'User Administrator'; // optional
+//        $admin->description  = 'User is allowed to manage and edit other users'; // optional
+//        $admin->save();
         return view('home');
     }
 }
